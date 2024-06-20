ace6afa51646dffb7fbf11821c656d0e7eaf24ce
wonuk(du_ck) <wonuk007@naver.com>
2024-06-20 15:16:59 +0900

    포인트 충전 비즈니스로직 작성

--- src/main/java/io/hhplus/tdd/service/PointService.java
+++ src/main/java/io/hhplus/tdd/service/PointService.java
@@ -1,10 +1,15 @@
 package io.hhplus.tdd.service;
 
 import io.hhplus.tdd.dto.UserPointDTO;
+import io.hhplus.tdd.entity.UserPoint;
+import io.hhplus.tdd.entity.types.TransactionType;
+import io.hhplus.tdd.exception.ChargeException;
 import io.hhplus.tdd.repository.PointHistoryRepository;
 import io.hhplus.tdd.repository.UserPointRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +18,26 @@ public class PointService {
     private final PointHistoryRepository pointHistoryRepository;
 
     public UserPointDTO chargePoint(long id, long amount) throws Exception {
+        if (amount < 0) {
+            throw new ChargeException("Amount must be positive");
+        }
+
+        //기존 포인트를 가져온다.
+        Optional<UserPoint> existUser = Optional.ofNullable(userPointRepository.selectById(id));
+        if (existUser.isPresent()) {
+            //update 시킬 point
+            Long updatePoint = existUser.get().point() + amount;
+
+            Optional<UserPoint> getPoint = Optional.ofNullable(userPointRepository.insertOrUpdate(id, updatePoint));
+            if (getPoint.isPresent()) {
+                pointHistoryRepository.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+                return UserPointDTO.builder()
+                        .user_id(getPoint.get().id())
+                        .user_point(getPoint.get().point())
+                        .build();
+            }
+        }
         return null;
     }
 }
